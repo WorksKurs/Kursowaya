d51f542b0f441a5d9995db2ea64c28760f8e7268
MrWISKI <VTV1994@Mail.ru>
2017-06-09 15:11:47 +0700

    Add calculation of generations

--- src/function.c
+++ src/function.c
@@ -26,18 +26,45 @@ void FillRand (int mass[], int sizex, int sizey) {
 
 void Game (int sizex, int sizey) {
     system ("clear");
-    int i, j;
+    int gen=0, i, j, neighbors, alive=1;
     sizex+=2;
     sizey+=2;
-    int mass[sizex*sizey];
+    int mass[sizex*sizey], step[sizex*sizey], pass1[sizex*sizey], pass2[sizex*sizey];
     FillNull (mass, sizex, sizey);
+    FillNull (step, sizex, sizey);
+    FillNull (pass1, sizex, sizey);
+    FillNull (pass2, sizex, sizey);
     FillRand (mass, sizex, sizey);
-    sleep (1);
-    for (i=1; i<sizey-1; i++) {
-        for (j=1; j<sizex-1; j++) {
-            if (mass[i*sizex+j]==0) printf ("|+|");
-            else printf ("| |");
+    while (alive!=0) {
+        alive=0;
+        gen++;
+        for (i=1; i<sizey-1; i++) {
+            for (j=1; j<sizex-1; j++) {
+                if (mass[i*sizex+j]==1) {
+                    alive++;
+                    printf ("|+|");
+                }
+                else printf ("| |");
+            }
+            printf ("\n");
         }
+        for (i=1; i<sizey-1; i++) {
+            for (j=1; j<sizex-1; j++) {
+                neighbors=0;
+                neighbors+=mass[(i-1)*sizey+j-1]+mass[(i-1)*sizey+j]+mass[(i-1)*sizey+j+1];
+                neighbors+=mass[i*sizey+j-1]+mass[i*sizey+j+1];
+                neighbors+=mass[(i+1)*sizey+j-1]+mass[(i+1)*sizey+j]+mass[(i+1)*sizey+j+1];
+                step[i*sizey+j]=neighbors;
+            }
+        }
+        for (i=1; i<sizey-1; i++) {
+            for (j=1; j<sizex-1; j++) {
+                if (mass[i*sizex+j]==0 && step[i*sizex+j]==3) mass[i*sizex+j]=1;
+                if (mass[i*sizex+j]==1 && step[i*sizex+j]!=2 && step[i*sizex+j]!=3) mass[i*sizex+j]=0;
+            }
+        }
+        sleep (0);
         printf ("\n");
     }
+    printf ("Все клетки погибли. Жизнь существовала %d поколения(ий).\n", gen-1);
 }
